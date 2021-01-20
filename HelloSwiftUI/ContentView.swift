9233ca3652ef52f55e60a6e1a7157d594f1d5ac9
Julio Litzenberg <julio.litzenberg@gmail.com>
2021-01-20 14:32:51 +0100

    Initial Commit

---
+++ HelloSwiftUI/ContentView.swift
@@ -0,0 +1,18 @@
+// HelloSwiftUI
+// 
+// Created by Julio Litzenberg on 20.01.21.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,14 @@
+// HelloSwiftUI
+// 
+// Created by Julio Litzenberg on 20.01.21.
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
